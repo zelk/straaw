3c596ad0229e75d0314692a4e6c474c70c079bc2
Ricky Helgesson <ricky.helgesson@gmail.com>
2014-11-25 09:50:55 +0100

    Added Straaw.Framework.Logging.Windows

---
+++ Framework/Logging/Straaw.Framework.Logging.Windows/ConsoleLogWriter.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Straaw.Framework.Logging;
+
+namespace Straaw.Framework.Logging.Windows
+{
+	public class ConsoleLogWriter : LogWriter
+	{
+		public ConsoleLogWriter(LogManager logManager) : base(logManager)
+		{
+		}
+
+		protected override void WriteLine(string textLine, LogEvent logEvent)
+		{
+			Console.WriteLine(textLine);
+		}
+
+	}
+}
